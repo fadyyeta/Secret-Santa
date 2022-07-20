a03eca5dee844cce0501aa7b463d0ecfe5813270
Fady Yeta <fadyyeta@gmail.com>
2022-07-20 07:51:27 +0200

    add margin top if no parallax

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, useRef } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer/Footer.js";
 import styles from "../styles/Layout.module.css";
@@ -7,14 +7,18 @@ const Layout = ({ children, props }) => {
 	let [layoutOptions, setLayoutOptions] = useState({
 		withParallax: props.withParallax,
 	});
+	let containerClass = useRef(styles.main);
 	useEffect(() => {
 		setLayoutOptions({ withParallax: props.withParallax });
+	containerClass.current += layoutOptions.withParallax
+		? ""
+		: " " + styles.containerNoParallax;
 	}, []);
 	return (
 		<Fragment>
 			<Nav withParallax={layoutOptions.withParallax} {...props} />
 			<div className={styles.containerFluid}>
-				<main className={styles.main}>{children}</main>
+				<main className={containerClass.current}>{children}</main>
 			</div>
 			<Footer />
 		</Fragment>
